d9aa349e5a5cf1997c3a52653357a178c2508dfd
Shahu Shinde <63205539+shahushinde18@users.noreply.github.com>
2026-08-25 22:39:44 +0530

    add privacy policy

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -65,3 +65,7 @@ func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file(
 		"res://Scenes/MainMenu/MainMenu.tscn"
 	)
+
+
+func _on_privacy_policy_pressed() -> void:
+	OS.shell_open("https://shahushinde18.github.io/pairnix-privacy/")
